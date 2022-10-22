d7a0744db4a2fcd9dc4baafb37619819f7bab7b8
David L. Inman <davidlevere@gmail.com>
2022-10-22 16:00:28 -0400

    window event convertion

--- src/event.rs
+++ src/event.rs
@@ -1,3 +1,5 @@
+use graphics_binding::sdl::event::*;
+use graphics_binding::sdl::window::*;
 
 #[derive(Debug)]
 pub struct Timestamp(pub u32);
@@ -20,9 +22,45 @@ pub enum Window {
     Close(Timestamp),
     TakeFocus(Timestamp),
     HitTest(Timestamp),
+    IccProfChanged(Timestamp),
+    DisplayChanged(Timestamp),
 }
 
 #[derive(Debug)]
 pub enum Event {
     Window(Window),
+    Quit,
+    Ignore,
+}
+
+pub fn convert(event : SDL_Event) -> Event {
+    unsafe {
+        match event.event_type {
+            SDL_WINDOWEVENT => {
+                let timestamp = Timestamp(event.window.timestamp);
+                let window = match event.window.event {
+                    SDL_WINDOWEVENT_SHOWN => Window::Shown(timestamp),
+                    SDL_WINDOWEVENT_HIDDEN => Window::Hidden(timestamp),
+                    SDL_WINDOWEVENT_EXPOSED => Window::Exposed(timestamp),
+                    SDL_WINDOWEVENT_MOVED => Window::Moved { timestamp, x: event.window.data_1, y: event.window.data_2 },
+                    SDL_WINDOWEVENT_RESIZED => Window::Resized { timestamp, width: event.window.data_1, height: event.window.data_2 },
+                    SDL_WINDOWEVENT_SIZE_CHANGED => Window::SizeChanged { timestamp, width: event.window.data_1, height: event.window.data_2 },
+                    SDL_WINDOWEVENT_MINIMIZED => Window::Minimized(timestamp),
+                    SDL_WINDOWEVENT_MAXIMIZED => Window::Maximized(timestamp),
+                    SDL_WINDOWEVENT_RESTORED => Window::Restored(timestamp),
+                    SDL_WINDOWEVENT_ENTER => Window::Enter(timestamp),
+                    SDL_WINDOWEVENT_LEAVE => Window::Leave(timestamp),
+                    SDL_WINDOWEVENT_FOCUS_GAINED => Window::FocusGained(timestamp),
+                    SDL_WINDOWEVENT_FOCUS_LOST => Window::FocusLost(timestamp),
+                    SDL_WINDOWEVENT_HIT_TEST => Window::HitTest(timestamp),
+                    SDL_WINDOWEVENT_ICCPROF_CHANGED => Window::IccProfChanged(timestamp),
+                    SDL_WINDOWEVENT_DISPLAY_CHANGED => Window::DisplayChanged(timestamp),
+                    _ => { return Event::Ignore },
+                };
+                Event::Window(window)
+            },
+            SDL_QUIT => Event::Quit,
+            _ => Event::Ignore,
+        }
+    }
 }
